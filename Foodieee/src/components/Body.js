7b398c92858ac87425b2aa7205f36e6a6aa0dfe7
Diksha Takyar <diksha.takyar@bikayi.com>
2023-08-07 13:07:23 +0530

    update

---
+++ Foodieee/src/components/Body.js
@@ -0,0 +1,48 @@
+import { restaurantList } from "../config";
+import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+
+const filterData = (searchInput, restaurants) => {
+  return restaurants.filter((restaurant) =>
+    restaurant.data.name.includes(searchInput)
+  );
+};
+
+const Body = () => {
+  const [searchInput, setSearchInput] = useState(""); //state variable
+  const [restaurants, setRestaurants] = useState(restaurantList);
+
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search..."
+          value={searchInput}
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchInput, restaurants);
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {restaurants.map((restaurant) => {
+          return (
+            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Body;
