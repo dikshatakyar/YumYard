98073b849947decbf23d7cccf9b543ea3234631d
Diksha Takyar <diksha.takyar@gmail.com>
2023-10-10 12:29:16 +0530

    res card

--- Foodieee/src/components/RestaurantCard.js
+++ Foodieee/src/components/RestaurantCard.js
@@ -3,15 +3,19 @@ import { IMG_CDN_URL } from "../config";
 const RestaurantCard = ({
   name,
   cuisines,
-  deliveryTime,
+  costForTwo,
   cloudinaryImageId,
+  avgRating,
+  sla,
 }) => {
   return (
     <div className="card">
       <img src={IMG_CDN_URL + cloudinaryImageId} />
       <h2>{name}</h2>
       <h3>{cuisines.join(", ")}</h3>
-      <h4>Delivery time : {deliveryTime} mins</h4>
+      <h3>{avgRating}</h3>
+      <h4>{costForTwo} </h4>
+      <h4>Delivery Time : {sla.deliveryTime} mins</h4>
     </div>
   );
 };
