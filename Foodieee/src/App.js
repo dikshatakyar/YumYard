f5015f1b025857e06a53f28e97605656601b54f8
Diksha Takyar <diksha.takyar@gmail.com>
2023-10-11 00:38:38 +0530

    app

--- Foodieee/src/App.js
+++ Foodieee/src/App.js
@@ -4,16 +4,43 @@ import "../Style.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 const Foodiez = () => {
   return (
     <>
       <Header />
-      <Body />
+      {/* <Body /> */}
+      <Outlet />
       <Footer />
     </>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Foodiez />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Foodiez />);
+root.render(<RouterProvider router={appRouter} />);
