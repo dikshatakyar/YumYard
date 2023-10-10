2cea59cb1b186893d0a5d430b9e9015b525a11a6
Diksha Takyar <diksha.takyar@gmail.com>
2023-10-11 00:37:46 +0530

    about

---
+++ Foodieee/src/components/About.js
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <>
+      <h1>ABOUT US</h1>
+      <h3>
+        Our mission is to elevate the quality of life for the urban consumer
+        with unparalleled convenience. Convenience is what makes us tick. It's
+        what makes us get out of bed and say, "Let's do this."
+      </h3>
+    </>
+  );
+};
+
+export default About;
