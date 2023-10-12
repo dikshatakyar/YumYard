5f561d3312b4e1a28c2010a46851e876228fd03e
Diksha Takyar <diksha.takyar@gmail.com>
2023-10-12 22:30:40 +0530

    config

--- Foodieee/src/config.js
+++ Foodieee/src/config.js
@@ -1,2 +1,5 @@
 export const IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const ResMenu_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6315885&lng=77.28307649999999&restaurantId=";
