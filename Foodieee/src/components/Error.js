a215ed1828bcac1589a33aec17cceae33cf46465
Diksha Takyar <diksha.takyar@gmail.com>
2023-10-14 22:20:12 +0530

    error page

---
+++ Foodieee/src/components/Error.js
@@ -0,0 +1,17 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+  const err = useRouteError();
+  console.log(err);
+  return (
+    <>
+      <h1>OOPS !</h1>
+      <h2>Something went wrong! 🥺</h2>
+      <h3>
+        {err.status} : {err.statusText}
+      </h3>
+    </>
+  );
+};
+
+export default Error;
