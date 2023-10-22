a1be7aa42888fa8ad52f63cbd1c4ff0055ad0e4b
Diksha Takyar <diksha.takyar@gmail.com>
2023-10-22 16:08:13 +0530

    no result

---
+++ Foodieee/src/components/NoResult.js
@@ -0,0 +1,11 @@
+const NoResult = () => {
+    return (
+        <>
+            <h2> OOPS ! </h2>
+            <h3> NO RESULTS FOUND ! TRY SEARCHING FOR SOMETHING ELSE!</h3>
+        </>
+    )
+}
+
+
+export default NoResult;
