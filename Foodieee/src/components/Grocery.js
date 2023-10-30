6932fb26448b02a25b0f974d20a36131799b7cd0
Diksha Takyar <diksha.takyar@gmail.com>
2023-10-30 17:47:38 +0530

    grocery

---
+++ Foodieee/src/components/Grocery.js
@@ -0,0 +1,11 @@
+const Grocery = () => {
+  return (
+    <>
+      <h1>
+        This is a grocery page, and assume we have a lot of components here.
+      </h1>
+    </>
+  );
+};
+
+export default Grocery;
