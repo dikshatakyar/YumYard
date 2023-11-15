b9a5558668f21db188f4dd3376fb50f49d9b95f0
Diksha Takyar <diksha.takyar@gmail.com>
2023-11-15 17:26:10 +0530

    no result

--- Foodieee/src/components/NoResult.js
+++ Foodieee/src/components/NoResult.js
@@ -1,6 +1,6 @@
 const NoResult = () => {
     return (
-        <div className="noResult">
+        <div className="flex flex-col items-center mt-16">
             <h2> OOPS ! </h2>
             <h3> NO DISHES FOUND ! TRY SEARCHING FOR SOMETHING ELSE!</h3>
         </div>
