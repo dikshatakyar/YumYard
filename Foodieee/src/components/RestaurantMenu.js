67e09424e5420b13287b0c136fc1c708d7afa882
Diksha Takyar <diksha.takyar@gmail.com>
2023-12-08 00:39:31 +0530

    update

--- Foodieee/src/components/RestaurantMenu.js
+++ Foodieee/src/components/RestaurantMenu.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import { IMG_CDN_URL } from "../utils/config";
 import NoResult from "./NoResult";
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
@@ -26,35 +27,52 @@ const RestaurantMenu = () => {
   const { itemCards } =
     resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
 
+  // console.log(
+  //   "MENU LIST : ",
+  //   resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards
+  // );
+
+  const categories =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c.card?.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+
+  console.log(categories);
+
   return (
-    <div className="menu">
-      <div className="resDetails">
-        <div className="resName">
-          <h1>{name}</h1>
-          <h3>
+    <div className="flex flex-col">
+      <div className="flex justify-around mt-12">
+        <div className="flex flex-col justify-center">
+          <h1 className="text-2xl my-5 font-bold">{name}</h1>
+          <h3 className="text-lg font-semibold">
             {cuisines.join(", ")} : {costForTwoMessage}
           </h3>
-          <h3>Rating : {avgRating}</h3>
-          <h3>Location : {areaName + ", " + city} </h3>
+          <h3 className="m-0">Rating : {avgRating}</h3>
+          <h3 className="m-0">Location : {areaName + ", " + city} </h3>
         </div>
-        <div className="resImg">
+        <div className="w-[200px] h-auto object-cover">
           <img src={IMG_CDN_URL + cloudinaryImageId} />
         </div>
       </div>
-      {itemCards ? (
-        <div className="recommendedMenu">
+      {/* {itemCards ? (
+        <div className="flex flex-col justify-center items-center">
           <h1>MENU</h1>
-          <ul>
-            <div className="sub-head">
+          <div>
+            <div className="flex flex-row justify-between m-3 font-bold">
               <div>ITEMS</div>
               <div>PRICE</div>
             </div>
             {itemCards &&
               itemCards.map((item) => {
                 return (
-                  <li className="menu-item" key={item.card.info.id}>
-                    <div className="dishName">{item.card.info.name}</div>
-                    <div className="dishPrice">
+                  <li
+                    className="flex justify-between m-2"
+                    key={item.card.info.id}
+                  >
+                    <div className="flex flex-wrap">{item.card.info.name}</div>
+                    <div className="ml-8">
                       Rs.{" "}
                       {item.card.info.price
                         ? item.card.info.price / 100
@@ -63,11 +81,22 @@ const RestaurantMenu = () => {
                   </li>
                 );
               })}
-          </ul>
+          </div>
         </div>
       ) : (
         <NoResult />
-      )}
+      )} */}
+
+      <div className="text-center">
+        {categories.map((category) => {
+          return (
+            <RestaurantCategory
+              key={category?.card?.card?.title}
+              data={category?.card?.card}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
